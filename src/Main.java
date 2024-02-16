0625771b92b47ddd67163efe65fab12c9704eb90
Rohithv17 <rohithv1396@gmail.com>
2024-02-16 16:22:38 +0530

    program to remove consonants fro the string

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import com.bridgelabz.removeconsonants.RemoveConsonants;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        RemoveConsonants r = new RemoveConsonants();
+        String s = " hello have  a great day";
+        r.removeConsonants(s);
+        }
+    }
---
+++ src/com/bridgelabz/removeconsonants/RemoveConsonants.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.removeconsonants;
+
+public class RemoveConsonants {
+    public  void  removeConsonants(String s){
+
+        String s2=s.replaceAll("[AEIOUaeiou]","");
+        System.out.println(s2);
+
+         }
+
+        }
+
+
