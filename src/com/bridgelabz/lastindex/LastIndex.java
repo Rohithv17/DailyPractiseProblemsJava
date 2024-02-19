9afc434efded260d3950541f52b115db6cc03d3f
Rohithv17 <rohithv1396@gmail.com>
2024-02-19 16:31:55 +0530

    Program to get character at last index

---
+++ src/com/bridgelabz/lastindex/LastIndex.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.lastindex;
+
+public class LastIndex {
+    public void checkLastIndex(String s){
+        int lastindex=s.length()-1;
+        System.out.println(s.charAt(lastindex)); // prints character at last index of the string 
+    }
+}
